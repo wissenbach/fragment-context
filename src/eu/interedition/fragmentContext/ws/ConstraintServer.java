0cb431ce2db57cf7a1608528a43c874b54b9721e
moz <moritz@wissenba.ch>
2011-10-10 18:37:28 +0200

    web service help

--- src/eu/interedition/fragmentContext/ws/ConstraintServer.java
+++ src/eu/interedition/fragmentContext/ws/ConstraintServer.java
@@ -1,8 +1,6 @@
 package eu.interedition.fragmentContext.ws;
 
-import org.restlet.Application;
 import org.restlet.Component;
-import org.restlet.Restlet;
 import org.restlet.data.Protocol;
 import org.restlet.resource.Directory;
 import org.restlet.routing.Router;
@@ -15,7 +13,7 @@ public class ConstraintServer {
 		// Add a new HTTP server listening on port 8182.
 		component.getServers().add(Protocol.HTTP, 8182);
 		component.getClients().add(Protocol.FILE);
-//		component.getClients().add(Protocol.CLAP);
+		//component.getClients().add(Protocol.CLAP);
 		
 
 		// INDEX PAGE
@@ -26,13 +24,12 @@ public class ConstraintServer {
 		
 		final Router router = new Router(component.getContext().createChildContext());
 
-		
-		router.attach("/create", ConstraintFactory.class);
 		router.attach("/", new Directory(component.getContext().createChildContext(),
 				ROOT_URI));
-		component.getDefaultHost().attach("/oac-constraint", router);
-		
+
+		router.attach("/create", ConstraintFactory.class);
 		router.attach("/match", ConstraintMatcher.class);
+		
 		component.getDefaultHost().attach("/oac-constraint", router);
 		
 		component.start();
