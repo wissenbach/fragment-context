bbf418643b237de771ef8f1c31ce052cd88eedb7
moz <moritz@wissenba.ch>
2011-10-10 15:27:57 +0200

    text impl

--- src/eu/interedition/fragmentContext/ws/ConstraintServer.java
+++ src/eu/interedition/fragmentContext/ws/ConstraintServer.java
@@ -1,7 +1,10 @@
 package eu.interedition.fragmentContext.ws;
 
+import org.restlet.Application;
 import org.restlet.Component;
+import org.restlet.Restlet;
 import org.restlet.data.Protocol;
+import org.restlet.resource.Directory;
 import org.restlet.routing.Router;
 
 public class ConstraintServer {
@@ -11,7 +14,26 @@ public class ConstraintServer {
 
 		// Add a new HTTP server listening on port 8182.
 		component.getServers().add(Protocol.HTTP, 8182);
+		component.getClients().add(Protocol.FILE);
 
+		// INDEX PAGE
+		
+		// URI of the root directory.
+		final String ROOT_URI = "file://" + System.getProperty("user.dir")
+		+ "/static/";
+
+		// Create an application
+		Application application = new Application() {
+		    @Override
+		    public Restlet createInboundRoot() {
+		            return new Directory(getContext(), ROOT_URI);
+		    }
+		};
+
+		// Attach the application to the component and start it
+		component.getDefaultHost().attach(application);
+		
+		
 		final Router router = new Router(component.getContext().createChildContext());
 
 		router.attach("/create", ConstraintFactory.class);
